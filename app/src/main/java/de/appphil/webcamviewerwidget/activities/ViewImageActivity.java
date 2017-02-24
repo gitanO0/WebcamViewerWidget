a9ac6b9c279e98fef99eab80a37db3bf6e0854f2
PhilippDE <p.dolif@gmail.com>
2017-02-24 11:48:27 +0100

    Images get saved with the correct date

--- app/src/main/java/de/appphil/webcamviewerwidget/activities/ViewImageActivity.java
+++ app/src/main/java/de/appphil/webcamviewerwidget/activities/ViewImageActivity.java
@@ -2,9 +2,14 @@ package de.appphil.webcamviewerwidget.activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
@@ -16,6 +21,9 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import de.appphil.webcamviewerwidget.R;
 import uk.co.senab.photoview.PhotoViewAttacher;
@@ -96,12 +104,90 @@ public class ViewImageActivity extends Activity {
         if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
             iv.setDrawingCacheEnabled(true);
             Bitmap b = iv.getDrawingCache();
-            MediaStore.Images.Media.insertImage(getContentResolver(), b, "WebcamViewerWidget", "");
+            // TODO: get name of the webcam and set this name as the title or description of the saved image
+            insertImage(getContentResolver(), b, "WebcamViewerWidget", "WebcamViewerWidget");
             // give the user some feedback
             Toast.makeText(this, getResources().getString(R.string.image_saved), Toast.LENGTH_LONG).show();
         } else {
             // app has no permission so ask for it and try to get it
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    private String insertImage(ContentResolver contentResolver, Bitmap source, String title, String description) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+
+        Uri url = null;
+        String stringUrl = null;
+
+        try {
+            url = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            if (source != null) {
+                OutputStream outputStream = contentResolver.openOutputStream(url);
+                try {
+                    source.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+                } finally {
+                    outputStream.close();
+                }
+
+                long id = ContentUris.parseId(url);
+                Bitmap miniThumb = MediaStore.Images.Thumbnails.getThumbnail(contentResolver, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
+                storeThumbnail(contentResolver, miniThumb, id, 50F, 50F, MediaStore.Images.Thumbnails.MICRO_KIND);
+            } else {
+                contentResolver.delete(url, null, null);
+                url = null;
+            }
+        } catch (Exception e) {
+            if (url != null) {
+                contentResolver.delete(url, null, null);
+                url = null;
+            }
+        }
+
+        if (url != null) {
+            stringUrl = url.toString();
+        }
+
+        return stringUrl;
+    }
+
+    private Bitmap storeThumbnail(ContentResolver contentResolver, Bitmap source, long id, float width, float height, int kind) {
+        // matrix for scaling
+        Matrix matrix = new Matrix();
+
+        float scaleX = width / source.getWidth();
+        float scaleY = height / source.getHeight();
+
+        matrix.setScale(scaleX, scaleY);
+
+        Bitmap thumbnail = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+        ContentValues values = new ContentValues(4);
+        values.put(MediaStore.Images.Thumbnails.KIND, kind);
+        values.put(MediaStore.Images.Thumbnails.IMAGE_ID, (int)id);
+        values.put(MediaStore.Images.Thumbnails.HEIGHT, thumbnail.getHeight());
+        values.put(MediaStore.Images.Thumbnails.WIDTH, thumbnail.getWidth());
+
+        Uri url = contentResolver.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
+
+        try {
+            OutputStream outputStream = contentResolver.openOutputStream(url);
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+            return thumbnail;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
