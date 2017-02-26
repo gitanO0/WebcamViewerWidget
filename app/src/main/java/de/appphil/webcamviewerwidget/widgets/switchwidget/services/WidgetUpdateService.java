dda4471fb90267fffa3b0b5f6bd883c10518ad39
PhilippDE <p.dolif@gmail.com>
2017-02-26 21:08:19 +0100

    Code improvements

--- app/src/main/java/de/appphil/webcamviewerwidget/widgets/switchwidget/services/WidgetUpdateService.java
+++ app/src/main/java/de/appphil/webcamviewerwidget/widgets/switchwidget/services/WidgetUpdateService.java
@@ -119,49 +119,7 @@ public class WidgetUpdateService extends IntentService {
 
         remoteViews.setImageViewBitmap(R.id.widget_wv_iv, bitmap);
 
-        /*
-        Reload Button
-        */
-        Intent intentReload = new Intent(getApplicationContext(), WidgetUpdateService.class);
-        intentReload.putExtra("id", id);
-        PendingIntent piReload = PendingIntent.getService(getApplicationContext(), id, intentReload, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.widget_wv_btn_reload, piReload);
-
-        /*
-        Left
-        */
-        Intent intentLeft = new Intent(getApplicationContext(), WidgetSwitchLinkService.class);
-        intentLeft.putExtra("left", true);
-        intentLeft.putExtra("id", id);
-        PendingIntent piLeft = PendingIntent.getService(getApplicationContext(), -id, intentLeft, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.widget_wv_btn_left, piLeft);
-
-        /*
-        Right
-        */
-        Intent intentRight = new Intent(getApplicationContext(), WidgetSwitchLinkService.class);
-        intentRight.putExtra("left", false);
-        intentRight.putExtra("id", id);
-        PendingIntent piRight = PendingIntent.getService(getApplicationContext(), id, intentRight, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.widget_wv_btn_right, piRight);
-
-        /*
-        Config button
-        */
-        Intent intentConfig = new Intent(getApplicationContext(), SwitchWidgetConfigActivity.class);
-        intentConfig.putExtra("id", id);
-        PendingIntent piConfig = PendingIntent.getActivity(getApplicationContext(), id, intentConfig, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.widget_wv_btn_settings, piConfig);
-
-        /*
-        ImageView
-        */
-        //
-        //ViewImageActivity should be started when image view is clicked
-        Intent intentViewImage = new Intent(getApplicationContext(), ViewImageActivity.class);
-        intentViewImage.putExtra(ViewImageActivity.EXTRA_IMAGE_PATH, id + "/" + Vars.IMAGE_FILENAME);
-        PendingIntent piViewImage = PendingIntent.getActivity(getApplicationContext(), id, intentViewImage, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.widget_wv_iv, piViewImage);
+        setOnClickPendingIntents(remoteViews, id);
 
         appWidgetManager.updateAppWidget(id, remoteViews);
 
@@ -214,6 +172,12 @@ public class WidgetUpdateService extends IntentService {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
         RemoteViews remoteViews = new RemoteViews(getApplication().getPackageName(), R.layout.widget_switch);
 
+        setOnClickPendingIntents(remoteViews, id);
+
+        appWidgetManager.updateAppWidget(id, remoteViews);
+    }
+
+    private void setOnClickPendingIntents(RemoteViews remoteViews, int id) {
         /*
         Reload Button
         */
@@ -257,8 +221,6 @@ public class WidgetUpdateService extends IntentService {
         intentViewImage.putExtra(ViewImageActivity.EXTRA_IMAGE_PATH, id + "/" + Vars.IMAGE_FILENAME);
         PendingIntent piViewImage = PendingIntent.getActivity(getApplicationContext(), id, intentViewImage, PendingIntent.FLAG_CANCEL_CURRENT);
         remoteViews.setOnClickPendingIntent(R.id.widget_wv_iv, piViewImage);
-
-        appWidgetManager.updateAppWidget(id, remoteViews);
     }
 
 }
